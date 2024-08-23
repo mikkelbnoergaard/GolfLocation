e5d992d7fabc9f48a5001db1f2956c39469ed012
Mikkel Nørgaard <mikkel.brunstedt@hotmail.dk>
2024-08-23 17:54:07 +0200

    test

--- app/src/main/java/com/plcoding/backgroundlocationtracking/LocationService.kt
+++ app/src/main/java/com/plcoding/backgroundlocationtracking/LocationService.kt
@@ -50,11 +50,11 @@ class LocationService: Service() {
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         locationClient
-            .getLocationUpdates(10000L)
+            .getLocationUpdates(1000L)
             .catch { e -> e.printStackTrace() }
             .onEach { location ->
-                val lat = location.latitude.toString().takeLast(3)
-                val long = location.longitude.toString().takeLast(3)
+                val lat = location.latitude.toString()
+                val long = location.longitude.toString()
                 val updatedNotification = notification.setContentText(
                     "Location: ($lat, $long)"
                 )
